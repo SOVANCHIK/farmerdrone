b25ace0ef9082e953fe6003adfffd045193fbd11
SOVANCHIK <sovanhik@gmail.com>
2026-09-22 12:48:35 +0300

    Третий сейв

--- Save0/main.py
+++ Save0/main.py
@@ -1,34 +1,51 @@
 while True:
 	if get_pos_x() == 0 and get_pos_y() == 0 and can_harvest():
 		harvest()
-		plant(Entities.Grass)
+		#grass
 		move(North)
 	elif get_pos_x() == 0 and get_pos_y() == 1 and can_harvest():
 		harvest()
-		plant(Entities.Grass)
+		#grass
 		move(North)
 	elif get_pos_x() == 0 and get_pos_y() == 2 and can_harvest():
 		harvest()
-		plant(Entities.Grass)
+		#grass
 		move(North)
 	elif get_pos_x() == 0 and get_pos_y() == 3 and can_harvest():
 		harvest()
-		plant(Entities.Grass)
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		#grass
 		move(North)
 		move(East)
 	elif get_pos_x() == 1 and get_pos_y() == 0 and can_harvest():
 		harvest()
-		plant(Entities.Bush)
+		plant(Entities.Tree)
 		move(North)
 	elif get_pos_x() == 1 and get_pos_y() == 1 and can_harvest():
 		harvest()
+
 		plant(Entities.Bush)
 		move(North)
 	elif get_pos_x() == 1 and get_pos_y() == 2 and can_harvest():
 		harvest()
-		plant(Entities.Bush)
+		plant(Entities.Tree)
 		move(North)
 	elif get_pos_x() == 1 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 5 and can_harvest():
 		harvest()
 		plant(Entities.Bush)
 		move(North)
@@ -48,5 +65,100 @@ while True:
 	elif get_pos_x() == 2 and get_pos_y() == 3 and can_harvest():
 		harvest()
 		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+		move(East)
+	elif get_pos_x() == 3 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+		move(East)
+	elif get_pos_x() == 4 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+		move(East)
+	elif get_pos_x() == 5 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
 		move(North)
 		move(East)
